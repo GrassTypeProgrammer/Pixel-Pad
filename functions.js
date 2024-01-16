6b72464d28659071e56d756ce57819df4f69de45
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2024-01-16 08:53:05 +0000

    cells can be left clicked to add color (currently just blue) and right clicked to remove color.

--- functions.js
+++ functions.js
@@ -4,6 +4,7 @@ _width = 20;
 function setupCanvas(){
     const canvas = document.getElementById('canvas');
     
+    // TODO: Luke see why this isn't working. It is needed for resizing the grid
     // while(canvas.firstChild()){
     //     canvas.remove.firstChild;
     // }
@@ -14,6 +15,8 @@ function setupCanvas(){
 
         for (let y = 0; y < this._height; y++) {
             const cell = document.createElement('div');
+            cell.addEventListener('click', onClickCanvasCell);
+            cell.addEventListener('contextmenu', onRightClickCanvasCell, false);
             cell.classList.add('PixelPad_canvasCell');
 
             if(x == 0){
@@ -28,4 +31,15 @@ function setupCanvas(){
 
         canvas.appendChild(row);
     }
+}
+
+
+function onClickCanvasCell(e){
+    e.target.style.background = 'blue';
+}
+
+function onRightClickCanvasCell(e){
+    e.preventDefault();
+    e.target.style.background = 'none';
+    return false;
 }
