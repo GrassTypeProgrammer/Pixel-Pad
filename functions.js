8d3509fc4b9292c7554619bbdd5859fb0595acb4
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2024-01-16 08:33:07 +0000

    Fixed grid so each cell now has an appropriate border.

--- functions.js
+++ functions.js
@@ -8,13 +8,21 @@ function setupCanvas(){
     //     canvas.remove.firstChild;
     // }
 
-    for (let x = 0; x < this._width; index++) {
+    for (let x = 0; x < this._width; x++) {
         const row = document.createElement('div');
         row.classList.add('PixelPad_canvasRow');
 
         for (let y = 0; y < this._height; y++) {
             const cell = document.createElement('div');
             cell.classList.add('PixelPad_canvasCell');
+
+            if(x == 0){
+                cell.classList.add('border-top');
+            }
+            if(y == 0){
+                cell.classList.add('border-left');
+            }
+            
             row.appendChild(cell);
         }
 
