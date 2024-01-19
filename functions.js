356f57d1cd378024947369db02eb2b6fe8bb2b31
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2024-01-19 08:36:49 +0000

    The user can now change the size of the grid. There is a min and max value for height and width. Asymetical grids are allowed.

--- functions.js
+++ functions.js
@@ -1,37 +1,44 @@
 _height = 20;
+_maxHeight = 32;
+_minHeight = 1;
 _width = 20;
+_maxWidth = 64;
+_minWidth = 1;
+_cellSize = 25;
 _currentColor = 'blue';
 
 
+//#region initialise
 function initialise(){
     initialiseCanvas();
-    createColorBox('red', true);
-    createColorBox('blue');
-    createColorBox('green');
+    initialiseColorBar();
+    initialiseGridChanger();
 }
 
 function initialiseCanvas(){
     const canvas = document.getElementById('canvas');
+    canvas.style.width = `${_cellSize * _width}px`;
+    canvas.style.height = `${_cellSize * _height}px`;;
     
-    // TODO: Luke see why this isn't working. It is needed for resizing the grid
-    // while(canvas.firstChild()){
-    //     canvas.remove.firstChild;
-    // }
+    while(canvas.firstElementChild){
+        canvas.removeChild(canvas.firstElementChild);
+    }
 
-    for (let x = 0; x < this._width; x++) {
+    
+    for (let y = 0; y < _height; y++) {
         const row = document.createElement('div');
         row.classList.add('PixelPad_canvasRow');
 
-        for (let y = 0; y < this._height; y++) {
+        for (let x = 0; x < _width; x++) {
             const cell = document.createElement('div');
             cell.addEventListener('click', onClickCanvasCell);
             cell.addEventListener('contextmenu', onRightClickCanvasCell, false);
             cell.classList.add('PixelPad_canvasCell');
 
-            if(x == 0){
+            if(y == 0){
                 cell.classList.add('border-top');
             }
-            if(y == 0){
+            if(x == 0){
                 cell.classList.add('border-left');
             }
             
@@ -42,17 +49,68 @@ function initialiseCanvas(){
     }
 }
 
-
-function onClickCanvasCell(e){
-    e.target.style.background = _currentColor;
+function initialiseColorBar(){
+    createColorBox('red', true);
+    createColorBox('blue');
+    createColorBox('green');
 }
 
-function onRightClickCanvasCell(e){
-    e.preventDefault();
-    e.target.style.background = 'none';
-    return false;
+function initialiseGridChanger(){
+    const row = document.getElementById('row1');
+    
+    const inputFieldX = document.createElement('input');
+    inputFieldX.id = 'inputX';
+    inputFieldX.classList.add('PixelPad_input');
+    inputFieldX.value = _width;
+    
+    const inputFieldY = document.createElement('input');
+    inputFieldY.id = 'inputY';
+    inputFieldY.classList.add('PixelPad_input');
+    inputFieldY.value = _height;
+
+    const button = document.createElement('button');
+    button.classList.add('PixelPad_button');
+    button.addEventListener('click', changeCanvasSize);
+    button.innerText = 'Change Size';
+
+    row.appendChild(inputFieldX);
+    row.appendChild(inputFieldY);
+    row.appendChild(button);
 }
 
+//#endregion
+
+//#region canvas size
+function changeCanvasSize(){
+    _width = document.getElementById('inputX').value;
+
+    if(_width > _maxWidth){
+        _width = _maxWidth;
+        document.getElementById('inputX').value = _maxWidth;
+    }
+    else if(_width < _minWidth){
+        _width = _minWidth;
+        document.getElementById('inputX').value = _minWidth;
+    }
+
+    _height = document.getElementById('inputY').value;
+    
+    if(_height > _maxHeight){
+        _height = _maxHeight;
+        document.getElementById('inputY').value = _maxHeight;
+    }
+    else if(_height < _minHeight){
+        _height = _minHeight;
+        document.getElementById('inputY').value = _minHeight;
+    }
+    
+    initialiseCanvas();
+}
+
+
+//#endregion
+
+//#region colorBar
 function createColorBox(color, selected = false){
     const colorBar = document.getElementById('colorBar');
     const colorBox = document.createElement('button');
@@ -80,4 +138,18 @@ function unselectColorBoxes(){
         boxes[i].classList.remove('selected');
         
     }
+}
+//#endregion
+
+
+
+
+function onClickCanvasCell(e){
+    e.target.style.background = _currentColor;
+}
+
+function onRightClickCanvasCell(e){
+    e.preventDefault();
+    e.target.style.background = 'none';
+    return false;
 }
